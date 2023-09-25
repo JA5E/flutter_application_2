208895e71917bb815318187fbaefd328ab34dc62
JASE <jase0485@gmail.com>
2023-09-25 15:41:59 -0600

    Add background image and card container to login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/widgets/auth_background.dart';
+import 'package:flutter_application_2/widgets/card_container.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,39 +16,71 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [_loginForm()],
+      body: AuthBackground(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 250,
+            ),
+            CardContainer(
+                child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Text("Login", style: Theme.of(context).textTheme.headlineSmall),
+                const SizedBox(
+                  height: 20,
+                ),
+                _loginForm(),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text(
+                  "Sign in or create a new account",
+                  style: TextStyle(fontSize: 14),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+              ],
+            )),
+          ],
         ),
-      ),
+      )),
     );
   }
 
   Widget _loginForm() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        TextField(
-          controller: _usernameController,
-          decoration: const InputDecoration(
-              icon: Icon(Icons.mail), labelText: 'Aqui pon tu usuario'),
-        ),
-        TextField(
-          controller: _passwordController,
-          decoration: const InputDecoration(
-              icon: Icon(Icons.lock_open), labelText: 'Aqui pon tu password'),
-          obscureText: true,
-          keyboardType: TextInputType.visiblePassword,
-        ),
-        Container(
-          margin: const EdgeInsets.only(top: 10.0),
-          child: ElevatedButton(
-            onPressed: onLogin,
-            child: const Text('Login'),
+    var column = Container(
+      padding: const EdgeInsets.all(30.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            controller: _usernameController,
+            decoration: const InputDecoration(
+                icon: Icon(Icons.mail), labelText: 'Aqui pon tu usuario'),
           ),
-        )
-      ],
+          TextField(
+            controller: _passwordController,
+            decoration: const InputDecoration(
+                icon: Icon(Icons.lock_open), labelText: 'Aqui pon tu password'),
+            obscureText: true,
+            keyboardType: TextInputType.visiblePassword,
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10.0),
+            child: ElevatedButton(
+              onPressed: onLogin,
+              child: const Text('Login'),
+            ),
+          )
+        ],
+      ),
     );
+    return column;
   }
 
   void onLogin() {
---
+++ lib/widgets/auth_background.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class AuthBackground extends StatelessWidget {
+  final Widget child;
+
+  const AuthBackground({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Stack(children: [_PurpleBox(), _HeaderIcon(), this.child]),
+    );
+  }
+}
+
+class _HeaderIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(top: 30),
+      child: const Icon(
+        Icons.account_circle,
+        color: Colors.white,
+        size: 150,
+      ),
+    ));
+  }
+}
+
+class _PurpleBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height * .4,
+      decoration: _purpleBackground(),
+      child: Stack(
+        children: [
+          Positioned(
+            child: _Bubble(),
+            top: 90,
+            left: 30,
+          ),
+          Positioned(
+            child: _Bubble(),
+            top: -40,
+            left: -30,
+          ),
+          Positioned(
+            child: _Bubble(),
+            top: -50,
+            left: -20,
+          ),
+          Positioned(
+            child: _Bubble(),
+            top: -50,
+            left: 10,
+          ),
+          Positioned(
+            child: _Bubble(),
+            top: 120,
+            left: 20,
+          ),
+        ],
+      ),
+    );
+  }
+
+  BoxDecoration _purpleBackground() => const BoxDecoration(
+      gradient: LinearGradient(
+          colors: [Color(0xFFB500AF), Color.fromRGBO(90, 70, 178, 1)]));
+}
+
+class _Bubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: const Color.fromRGBO(255, 255, 255, .5),
+      ),
+    );
+  }
+}
