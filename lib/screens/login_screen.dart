4c01051c0a23b2ae5e55da4f568c21411af0c412
JASE <jase0485@gmail.com>
2023-10-09 15:28:34 -0600

    LoginForm wrapped in a ChangeNotifierProvider

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -36,7 +36,10 @@ class _LoginScreenState extends State<LoginScreen> {
                 const SizedBox(
                   height: 20,
                 ),
-                _loginForm(),
+                ChangeNotifierProvider(
+                  create: (_) => LoginFormProvider(),
+                  child: _loginForm(),
+                ),
                 const SizedBox(
                   height: 50,
                 ),
@@ -54,8 +57,12 @@ class _LoginScreenState extends State<LoginScreen> {
       )),
     );
   }
+}
 
-  Widget _loginForm() {
+// ignore: camel_case_types
+class _loginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     //flutter pub add provider
     final loginForm = Provider.of<LoginFormProvider>(context);
     return Form(
@@ -65,27 +72,61 @@ class _LoginScreenState extends State<LoginScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           TextFormField(
-            controller: _usernameController,
             decoration: InputDecorations.authInputDecoration(
                 hintText: "Ingresa un nombre propio solamente sin apellidos",
                 labelText: 'Aqui pon tu usuario',
                 prefixIcon: Icons.email),
-            validator: (value) {},
+            validator: (value) {
+              String pattern = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
+              RegExp regExp = new RegExp(pattern);
+
+              return regExp.hasMatch(value ?? '')
+                  ? null
+                  : 'Input a valid email';
+            },
           ),
-          TextField(
-            controller: _passwordController,
+          TextFormField(
             decoration: InputDecorations.authInputDecoration(
                 hintText: "Debe tener letras y numeros",
                 prefixIcon: Icons.lock_open,
                 labelText: 'Aqui pon tu password'),
+            validator: (value) {
+              return (value != null && value.length >= 6)
+                  ? null
+                  : 'Password must be at least six characters long.';
+            },
             obscureText: true,
             keyboardType: TextInputType.visiblePassword,
           ),
           Container(
             margin: const EdgeInsets.only(top: 10.0),
-            child: ElevatedButton(
-              onPressed: onLogin,
-              child: const Text('Login'),
+            child: MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(1)),
+              elevation: 0,
+              disabledColor: const Color(0xFF575757),
+              color: const Color.fromARGB(255, 107, 6, 121),
+              onPressed: loginForm.isLoading
+                  ? null
+                  : () async {
+                      FocusScope.of(context).unfocus();
+
+                      if (!loginForm.isValidForm()) {
+                        return;
+                      } else {
+                        loginForm.isLoading = true;
+
+                        await Future.delayed(Duration(seconds: 2));
+
+                        loginForm.isLoading = false;
+
+                        Navigator.pushReplacementNamed(context, 'home');
+                      }
+                    },
+              child: Text(
+                loginForm.isLoading ? 'wait' : 'Sign in',
+                style: const TextStyle(color: Color(0xFFC4C4C4)),
+              ),
             ),
           )
         ],
@@ -94,10 +135,10 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   void onLogin() {
-    final username = _usernameController.text.trim();
-    final password = _passwordController.text.trim();
+    //final username = _usernameController.text.trim();
+    //final password = _passwordController.text.trim();
 
-    print('username: $username');
-    print('password: $password');
+    //print('username: $username');
+    //print('password: $password');
   }
 }
